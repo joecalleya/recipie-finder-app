89e7b7614ad833669bbc40303eb90aaf2898953f
Joe Calleya <jcalleya@gmail.com>
2020-10-20 09:28:08 +0100

    yarn iusse

--- src/components/RecipieCard/RecipieCard.jsx
+++ src/components/RecipieCard/RecipieCard.jsx
@@ -4,15 +4,12 @@ import styles from "./RecipieCard.module.scss";
 
 const RecipieCard = (props) => {
      const { recipe } = props;
-    //  const [props, setProps] = useState([]);
-    //  setRecipes(props)
-    //  const {idMeal, strMeal, } = recipe;
-    // {recipe.map(item => <div>{item.strMeal}</div>)}
+
     console.log("props",recipe)
 
             return (
                 <div className={styles.RecipieCard}>
-                    {recipe.strMeal}
+                    {recipe.strInstructions}
                 </div>
                     )
         };
